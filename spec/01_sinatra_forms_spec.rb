f4d468c9bdac272444aa62e0619b4b5670df2374
Adam Weissman <adam@comedytragedyepic.com>
2019-12-21 13:10:33 +0000

    Automatically backed up by Learn

--- spec/01_sinatra_forms_spec.rb
+++ spec/01_sinatra_forms_spec.rb
@@ -46,9 +46,9 @@ describe "FormsLab::App" do
     end
   end
 
-  describe "POST '/pirates'" do
+  describe "POST '/shiiit'" do
     before do
-      post '/pirates', {
+      post '/shiiit', {
         "pirate"=> {
           "name"=>"Ian",
           "weight"=>"165",
