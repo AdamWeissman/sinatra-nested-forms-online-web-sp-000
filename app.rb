af8cf09eb8bb06601a1049a0dc213284dfa51b6e
Adam Weissman <adam@comedytragedyepic.com>
2019-12-21 13:12:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,8 @@ module FormsLab
       erb :'pirates/new'
     end
 
-#shiiit was originally pirate ... so, essentially, you can call your route whatever you want.  When you are doing gets and posts in the app.rb file, you are DEFINING the route, not calling it something that already existed.
+#shiiit was originally pirate ... so, essentially, you can call your route whatever you want.  When you are doing gets and posts in the app.rb file, you are DEFINING the route, not calling it something that already existed.]
+# Really annoying though that I wouldn't have been able to edit this without editing the RSPEC test to accept shiiit as opposed to pirate.
 
     post '/shiiit' do
       @pirate = Pirate.new(params[:pirate])
